7d5635cf4bb1b3ee726520377f2a07d695a5b365
Crensiglova <51062332+Crensiglova@users.noreply.github.com>
2019-08-08 15:54:08 -0300

    Atualizado 08/08/2019

--- js/efeitos.js
+++ js/efeitos.js
@@ -64,6 +64,29 @@
 
 		  	});
 
+		  	$("#btn-bars").on("click",function(){
+
+		  		$("header").toggleClass("open-menu");
+
+		  	});
+
+
+		  	$("#menu-mobile-mask, .btn-close").on("click",function(){
+
+		  		$("header").removeClass("open-menu");
+
+
+		  	});
+
+		$("#btn-search").on("click",function(){
+
+			$("header").toggleClass("open-search");
+		  	$("#input-search-mobile").focus();
+
+
+		});  	
+		  	
+
 
 		});
 		
